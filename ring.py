fc7ae72cd348ba6bec419ba762bfeb6191d74411
viktorepo <38084244+viktorepo@users.noreply.github.com>
2019-10-01 11:02:22 -0500

    ring 10/01

---
+++ ring.py
@@ -0,0 +1,16 @@
+import turtle
+wn = turtle.Screen()
+wn.bgcolor('grey')
+Ring = turtle.Turtle()
+Ring.speed(0)
+Ring.pensize(1)
+Ring.color('black')
+def drawCircle(t,size):
+    for i in range(15):
+        t.circle(size)
+        size=size-5
+def draw(t,size,repeat):
+  for i in range (repeat):
+  	drawCircle(t,size)
+  	t.right(360/repeat)
+draw(Ring,100,8)
